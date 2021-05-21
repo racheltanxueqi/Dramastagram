1c346c14c817c84fb9b84d581d1406de0608de08
Rachel Tan Xue Qi <e0191632@u.nus.edu>
2021-05-21 23:25:20 +0800

    add swipe feature

---
+++ src/pages/dashboard/index.js
@@ -0,0 +1,76 @@
+import TinderCard from 'react-tinder-card'
+import React, { useState, useMemo } from 'react'
+
+const db = [
+    {
+      name: 'Richard Hendricks',
+      url: './img/richard.jpg'
+    },
+    {
+      name: 'Erlich Bachman',
+      url: './img/erlich.jpg'
+    },
+    {
+      name: 'Monica Hall',
+      url: './img/monica.jpg'
+    },
+    {
+      name: 'Jared Dunn',
+      url: './img/jared.jpg'
+    },
+    {
+      name: 'Dinesh Chugtai',
+      url: './img/dinesh.jpg'
+    }
+  ]
+  
+const alreadyRemoved = []
+let charactersState = db // This fixes issues with updating characters state forcing it to use the current state and not the state that was active when the card was created.
+  
+function Dashboard() {
+    const [characters, setCharacters] = useState(db)
+    const [lastDirection, setLastDirection] = useState()
+  
+    const childRefs = useMemo(() => Array(db.length).fill(0).map(i => React.createRef()), [])
+  
+    const swiped = (direction, nameToDelete) => {
+      console.log('removing: ' + nameToDelete)
+      setLastDirection(direction)
+      alreadyRemoved.push(nameToDelete)
+    }
+  
+    const outOfFrame = (name) => {
+      console.log(name + ' left the screen!')
+      charactersState = charactersState.filter(character => character.name !== name)
+      setCharacters(charactersState)
+    }
+  
+    const swipe = (dir) => {
+      const cardsLeft = characters.filter(person => !alreadyRemoved.includes(person.name))
+      if (cardsLeft.length) {
+        const toBeRemoved = cardsLeft[cardsLeft.length - 1].name // Find the card object to be removed
+        const index = db.map(person => person.name).indexOf(toBeRemoved) // Find the index of which to make the reference to
+        alreadyRemoved.push(toBeRemoved) // Make sure the next card gets removed next time if this card do not have time to exit the screen
+        childRefs[index].current.swipe(dir) // Swipe the card!
+      }
+    }
+    return (
+        <div>
+            <div className='cardContainer'>
+                {characters.map((character, index) =>
+                <TinderCard ref={childRefs[index]} className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+                    <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
+                    <h3>{character.name}</h3>
+                    </div>
+                </TinderCard>
+                )}
+            </div>
+            <div className='buttons'>
+                <button onClick={() => swipe('left')}>Not My Taste</button>
+                <button onClick={() => swipe('right')}>Interested</button>
+            </div>
+            {lastDirection ? <h3 key={lastDirection} className='infoText'>You swiped {lastDirection === "left" ? "Not My Taste" : "Interested"}!</h3> : <h3 className='infoText'>Swipe a card or press a button to get started!</h3>}
+        </div>    )
+}
+
+export default Dashboard;
