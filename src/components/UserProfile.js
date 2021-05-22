8c6427991de49fdf471223b2b1b28fc998811022
Wei Hao <weihao.tan@zendesk.com>
2021-05-22 18:17:13 +0800

    Add profile

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
-function UserProfile() {
+function UserProfile(props) {
+    const {user} = props
+    const { name, following, followers } = props.user
     return (
         <div className="d-flex justify-content-center">
             <div className="profile-card p-4">
                 <div className="d-flex align-items-center">
                     <div className="image"> <img src="https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=80" className="rounded" width="155" /> </div>
                     <div className="ml-3 w-100">
-                        <h4 className="mb-0 mt-0">Alex Morrision</h4>
+                        <h4 className="mb-0 mt-0">{name || '' }</h4>
                         <div className="p-2 mt-2 bg-light d-flex justify-content-between rounded text-white stats">
-                            <div className="d-flex flex-column"> <span className="followings">Followings</span> <span className="number1">38</span> </div>
-                            <div className="d-flex flex-column"> <span className="followers">Followers</span> <span className="number2">980</span> </div>
+                            <div className="d-flex flex-column"> <span className="followings">Followings</span> <span className="number1">{following?.length || 0}</span> </div>
+                            <div className="d-flex flex-column"> <span className="followers">Followers</span> <span className="number2">{followers?.length || 0}</span> </div>
                             {/* <div className="d-flex flex-column"> <span className="similarity">Similarity</span> <span className="number3">8.9</span> </div> */}
                         </div>
                         <div className="button mt-2 d-flex flex-row align-items-center">
--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import UserProfile from '../../components/UserProfile';
 import TabsProfile from '../../components/TabsProfile';
 
+import { db } from '../../firebase/firebase'
+import useAuth from '../../firebase/firebaseAuth'
+
 function Profile() {
+    const { pending, isSignedIn, user } = useAuth()
+    const [currentUser, setCurrentUser] = useState({})
+   
+    useEffect(() => {
+      if (!pending & isSignedIn) {
+        const unsubscribe = db
+          .collection('users')
+          .doc('CZMk95xeqPbiFBSiwcDt1nGq2Hs2')
+          .onSnapshot(doc => {
+            setCurrentUser(doc.data())
+          })
+        return () => {
+            unsubscribe()
+        }
+      } 
+    }, [pending, isSignedIn, user])
+    
+    console.log(currentUser)
     return(
         <div>
-            <UserProfile />
+            <UserProfile user={currentUser} />
             <TabsProfile  />
         </div>
     )
